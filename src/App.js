b5419b9324054e48cab3c495e9d7784e467fe683
Milind Kumar Tyagi <milind1179@gmail.com>
2026-06-03 13:31:52 +0530

    SPR-1 testing 6

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <main className="app-cardd">
         <span className="app-baddge">React App</span>
         <h1>Dashboard Tracker Testing hello </h1>
-        <p>Your repository has been initia5lized as a standard React application.</p>
+        <p>Your repository has been initia5lizedt as a standard React application.</p>
       </main>
     </div>
   );
