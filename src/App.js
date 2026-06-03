36b7bedebe5714a1c2f31477d86595aac8986d80
Milind Kumar Tyagi <milind1179@gmail.com>
2026-06-03 13:48:11 +0530

    SPR-1 testing 6

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="app-shellll">
       <main className="app-cardd">
         <span className="app-baddge">React App</span>
-        <h1>Dashboard Tracker Testing hello </h1>
+        <h1>Dashboard Tracker Testing ehello </h1>
         <p>Your repository has been binitia5lizedt as a s tandard React application.</p>
       </main>
     </div>
